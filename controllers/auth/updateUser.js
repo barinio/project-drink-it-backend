3c72d74a662760964dae19072e9e91ee0c670bda
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2024-02-02 18:28:29 +0200

    Update updateUser.js

--- controllers/auth/updateUser.js
+++ controllers/auth/updateUser.js
@@ -3,24 +3,24 @@ const { HttpError } = require('../../helpers');
 const bcrypt = require('bcrypt');
 
 const updateUser = async (req, res) => {
-	const { id } = req.params;
+	const { _id } = req.user;
 	const {
-		_id,
 		email,
-		outdatedPassword,
 		userName,
 		dailyNorma,
 		avatarURL,
 		gender,
+		outdatedPassword,
 		newPassword,
 		repeatedNewPassword,
 	} = req.body;
 
-	const user = await User.findById(id);
+	const user = await User.findById(_id);
 
 	if (!user) {
 		throw HttpError(401, 'Email or password is wrong');
 	}
+	console.log('user.password:', user.password);
 
 	if (outdatedPassword) {
 		const passwordCompare = await bcrypt.compare(outdatedPassword, user.password);
@@ -31,52 +31,54 @@ const updateUser = async (req, res) => {
 
 	if (!outdatedPassword && newPassword && repeatedNewPassword && repeatedNewPassword !== newPassword) {
 		throw HttpError(401, 'new password and repeated new password must be the same');
-	} else if (newPassword && repeatedNewPassword && repeatedNewPassword === newPassword) {
-		const newHashPassword = await bcrypt.hash(newPassword, 10);
-		console.log(newHashPassword);
-		user.password = newHashPassword;
-		console.log(user.password);
-	} else {
-		res.json({ user });
 	}
 
-	await User.findByIdAndUpdate(id, { password: user.password });
+	if (newPassword && repeatedNewPassword && repeatedNewPassword === newPassword) {
+		const newHashPassword = await bcrypt.hash(newPassword, 10);
+		console.log('newHashPassword:', newHashPassword);
+		user.password = newHashPassword;
+		console.log('user.password:', user.password);
+	}
 
-	// const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
-	// if (!updatedUser) {
-	// 	throw HttpError(404, 'Not found');
-	// }
+	await User.findByIdAndUpdate(
+		_id,
+		{
+			password: user.password,
+			email,
+			userName,
+			dailyNorma,
+			avatarURL,
+			gender,
+			// outdatedPassword: user.outdatedPassword,
+			// newPassword: user.newPassword,
+			// repeatedNewPassword: user.repeatedNewPassword,
+		},
+		{ new: true }
+	);
 
-	// if (outdatedPassword) {
-	// 	const passwordCompare = await bcrypt.compare(outdatedPassword, updatedUser.password);
-	// 	if (!passwordCompare) {
-	// 		throw HttpError(401, 'outdated password is wrong');
-	// 	}
-	// }
-
-	// console.log(updatedUser.outdatedPassword);
-	// console.log(updatedUser.newPassword);
-	// console.log(updatedUser.repeatedNewPassword);
-
-	// const hashPassword = await bcrypt.hash(repeatedNewPassword, 10);
-	// console.log(hashPassword);
-	// console.log(updatedUser.password);
-	// updateUser.password = hashPassword;
-	// // updatedUser.password = hashPassword;
-	// console.log(updateUser.password);
-
-	// if (newPassword && repeatedNewPassword && repeatedNewPassword !== newPassword) {
-	// 	throw HttpError(401, 'new password and repeated new password must be the same');
-	// } else if (newPassword && repeatedNewPassword && repeatedNewPassword === newPassword) {
-	// 	const hashPassword = await bcrypt.hash(newPassword, 10);
-	// 	updatedUser.password = hashPassword;
-	// 	console.log(updatedUser.password);
-	// } else {
-	// 	res.json({ _id, email, userName, dailyNorma, avatarURL, gender });
-	// }
-
-	res.json({ user });
-	// res.json({ _id, email, userName, dailyNorma, avatarURL, gender });
+	res.json({
+		_id,
+		email,
+		userName,
+		dailyNorma,
+		avatarURL,
+		gender,
+		outdatedPassword,
+		newPassword,
+		repeatedNewPassword,
+	});
 };
 
 module.exports = updateUser;
+
+// console.log(outdatedPassword);
+// console.log(user.password);
+// const hashPasswordNew = await bcrypt.hash(outdatedPassword, 10);
+// console.log(outdatedPassword);
+
+// const passwordCompare = await bcrypt.compare(outdatedPassword, hashPasswordNew);
+// console.log(passwordCompare);
+
+// res.json({ user });
+
+// await User.findByIdAndUpdate(id, { password: user.password });
