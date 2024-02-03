b756271513296485dc0557c556a21737a66be9cc
KaterinichViktor <vspasedust@gmail.com>
2024-02-04 00:08:40 +0200

    dailyNorma

--- controllers/auth/getDailyNorma.js
+++ controllers/auth/getDailyNorma.js
@@ -1,19 +1,15 @@
 const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 // const { schemas } = require('../models/userModel');
-const { validateID } = require('../../service/waterServices/uuidValid');
 
 const getDailyNorma = async (req, res) => {
   try {
     // Retrieve user's information including dailyNorma from the database
-    const { _id } = req.query;
-    const { id } = req.params;
+    const { _id } = req.user;
 
-    const validid = validateID(_id);
+    const user = await User.findById(_id);
 
-    const user = await User.findById(id);
-
-    if (!edit || !validid) {
+    if (!edit) {
       throw HttpError(404, 'User not found');
     }
 
