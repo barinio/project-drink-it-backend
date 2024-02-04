f31f30425572f798da30e79afcdef7ff82f72d62
KaterinichViktor <vspasedust@gmail.com>
2024-02-05 00:37:17 +0200

    dailynorma

--- controllers/auth/getDailyNorma.js
+++ controllers/auth/getDailyNorma.js
@@ -1,31 +1,27 @@
 const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 
+const { User } = require('../../models');
+const { HttpError } = require('../../helpers');
+const { fetchDailyNorma} = require('../services/api');
+
 const getDailyNorma = async (req, res) => {
   try {
-    const { id } = req.params;
-
-    const user = await User.findById( { _id: id });
+    const { _id } = req.user;
+    const user = await User.findById(_id);
 
     if (!user) {
       throw HttpError(404, 'User not found');
     }
 
-    const {
-      dailyNorma = 0,
-      weight = 0,
-      gender = '',
-      activityTime = 0,
-      willDrink = 0,
-    } = user;
+    const dailyNormaData = await fetchDailyNorma(userId);
 
-    // Return the extracted properties in the response
     res.status(200).json({
-      dailyNorma,
-      weight,
-      gender,
-      activityTime,
-      willDrink,
+      dailyNorma: dailyNormaData.dailyNorma || 0,
+      weight: user.weight || 0,
+      gender: user.gender || '',
+      activityTime: user.activityTime || 0,
+      willDrink: user.willDrink || 0,
     });
   } catch (error) {
     res.status(error.status || 500).json({
--- controllers/auth/updateDailyNorma.js
+++ controllers/auth/updateDailyNorma.js
@@ -17,47 +17,48 @@ const { HttpError } = require('../../helpers');
 const { updateDailyNorma } = require('../services/api');
 
 
+const { User } = require('../../models');
+const { HttpError } = require('../../helpers');
+const { updateDailyNorma } = require('../services/api');
+
+
 const updateDailyNorma = async (req, res) => {
-	try {
-	  const { _id } = req.user;
-	  const {
-		newDailyNorma,
-		newWeight,
-		newGender,
-		newActivityTime,
-		newWillDrink,
-	  } = req.body;
+  try {
+    const { _id } = req.user;
+    const { newDailyNorma, weight, gender, activityTime, willDrink } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      $set: {
+      dailyNorma: newDailyNorma,
+      weight: weight,
+      gender: gender,
+      activityTime: activityTime,
+      willDrink: willDrink,
+      },
+    },
+    { new: true }
+    );
   
-	  const updatedUser = await User.findByIdAndUpdate(
-		_id,
-		{
-		  $set: {
-			dailyNorma: newDailyNorma,
-			weight: newWeight,
-			gender: newGender,
-			activityTime: newActivityTime,
-			willDrink: newWillDrink,
-		  },
-		},
-		{ new: true }
-	  );
-  
-	  if (!updatedUser) {
-		throw HttpError(404, 'Not found');
-	  }
-  
-	  res.status(200).json({
-		updatedDailyNorma: updatedUser.dailyNorma || 0,
-		updatedWeight: updatedUser.weight || 0,
-		updatedGender: updatedUser.gender || '',
-		updatedActivityTime: updatedUser.activityTime || 0,
-		updatedWillDrink: updatedUser.willDrink || 0,
-	  });
-	} catch (error) {
-	  res.status(error.status || 500).json({
-		error: error.message || 'Internal Server Error',
-	  });
-	}
+    if (!updatedUser) {
+    throw HttpError(404, 'User not found');
+    }
+
+    res.status(200).json({
+    updatedUser: {
+      dailyNorma: updatedUser.dailyNorma || 0,
+      weight: updatedUser.weight || 0,
+      gender: updatedUser.gender || '',
+      activityTime: updatedUser.activityTime || 0,
+      willDrink: updatedUser.willDrink || 0,
+    },
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({
+    error: error.message || 'Internal Server Error',
+    });
+  }
   };
 
 module.exports = updateDailyNorma;
