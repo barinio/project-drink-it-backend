3bba38b113b74ccb88de0146418be56c3c72dbeb
Bohdan Izbash <olgaizbash@gmail.com>
2024-02-05 01:10:13 +0200

    ffix

--- controllers/auth/getDailyNorma.js
+++ controllers/auth/getDailyNorma.js
@@ -2,36 +2,30 @@ const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 
 const getDailyNorma = async (req, res) => {
-  try {
-    const { id } = req.params;
+	try {
+		const { id } = req.params;
 
-    const user = await User.findById( { _id: id });
+		const user = await User.findById({ _id: id });
 
-    if (!user) {
-      throw HttpError(404, 'User not found');
-    }
+		if (!user) {
+			throw HttpError(404, 'User not found');
+		}
 
-    const {
-      dailyNorma = 0,
-      weight = 0,
-      gender = '',
-      activityTime = 0,
-      willDrink = 0,
-    } = user;
+		const { dailyNorma = 2000, weight = 0, gender = '', activityTime = 0, willDrink = 0 } = user;
 
-    // Return the extracted properties in the response
-    res.status(200).json({
-      dailyNorma,
-      weight,
-      gender,
-      activityTime,
-      willDrink,
-    });
-  } catch (error) {
-    res.status(error.status || 500).json({
-      error: error.message || 'Internal Server Error',
-    });
-  }
+		// Return the extracted properties in the response
+		res.status(200).json({
+			dailyNorma,
+			weight,
+			gender,
+			activityTime,
+			willDrink,
+		});
+	} catch (error) {
+		res.status(error.status || 500).json({
+			error: error.message || 'Internal Server Error',
+		});
+	}
 };
 
 module.exports = getDailyNorma;
