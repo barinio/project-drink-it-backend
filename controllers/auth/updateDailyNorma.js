5af2deb480d807913011fafa78df3f63d1b95bd1
KaterinichViktor <vspasedust@gmail.com>
2024-02-05 00:48:41 +0200

    dailynorma

--- controllers/auth/updateDailyNorma.js
+++ controllers/auth/updateDailyNorma.js
@@ -17,42 +17,34 @@ const { HttpError } = require('../../helpers');
 const { updateDailyNorma } = require('../services/api');
 
 
-const { User } = require('../../models');
-const { HttpError } = require('../../helpers');
-const { updateDailyNorma } = require('../services/api');
-
-
 const updateDailyNorma = async (req, res) => {
   try {
     const { _id } = req.user;
-    const { newDailyNorma, weight, gender, activityTime, willDrink } = req.body;
+    const { newDailyNorma, newWeight, newGender, newActivityTime, newWillDrink } = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
     _id,
     {
-      $set: {
       dailyNorma: newDailyNorma,
-      weight: weight,
-      gender: gender,
-      activityTime: activityTime,
-      willDrink: willDrink,
+      weight: newWeight,
+      gender: newGender,
+      activityTime: newActivityTime,
+      willDrink: newWillDrink,
       },
-    },
     { new: true }
     );
   
-    if (!updatedUser) {
-    throw HttpError(404, 'User not found');
-    }
+    // if (!updatedUser) {
+    // throw HttpError(404, 'User not found');
+    // }
 
     res.status(200).json({
-    updatedUser: {
-      dailyNorma: updatedUser.dailyNorma || 0,
-      weight: updatedUser.weight || 0,
-      gender: updatedUser.gender || '',
-      activityTime: updatedUser.activityTime || 0,
-      willDrink: updatedUser.willDrink || 0,
-    },
+		_id: _id,
+    	dailyNorma: newDailyNorma,
+    	weight: newWeight,
+    	gender: newGender,
+    	activityTime: newActivityTime,
+    	willDrink: newWillDrink,
     });
   } catch (error) {
     res.status(error.status || 500).json({
