797dca1b33ff473b56f325afd5323affa5f93775
KaterinichViktor <vspasedust@gmail.com>
2024-02-06 21:31:24 +0200

    dailyNorma without get try1

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,5 +1,11 @@
+// const current = async (req, res) => {
+// 	const { _id, email, userName, dailyNorma, avatarURL, gender } = req.user;
+// 	res.json({ _id, email, userName, dailyNorma, avatarURL, gender });
+// };
+// module.exports = current;
+
 const current = async (req, res) => {
-	const { _id, email, userName, dailyNorma, avatarURL, gender } = req.user;
-	res.json({ _id, email, userName, dailyNorma, avatarURL, gender });
+	const { _id, email, userName, dailyNorma, avatarURL, gender, willDrink, activityTime, weight } = req.user;
+	res.json({ _id, email, userName, dailyNorma, avatarURL, gender, willDrink, activityTime, weight });
 };
 module.exports = current;
